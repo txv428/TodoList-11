a795f95be95d65629479f6237be71546d7e1e4f5
Tejasree V <tejasree.vangapelli@gmail.com>
2020-04-15 19:30:05 -0700

    Added search bar

--- TodoList/TodoViewController.swift
+++ TodoList/TodoViewController.swift
@@ -1,15 +1,18 @@
 //
-//  ToDoTableViewController.swift
+//  TodoViewController.swift
 //  TodoList
 //
-//  Created by Apple on 10/04/20.
+//  Created by Apple on 15/04/20.
 //  Copyright © 2020 Apple. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
-class ToDoTableViewController: UITableViewController {
+class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     
     let filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     let databaseContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -19,22 +22,26 @@ class ToDoTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         loadItems()
+        print(itemsModel)
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return itemsModel.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListCell", for: indexPath)
         let currItem = itemsModel[indexPath.row]
         cell.textLabel?.text = currItem.itemName
@@ -44,7 +51,7 @@ class ToDoTableViewController: UITableViewController {
     
     // MARK: - TableView Delegate methods
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemsModel[indexPath.row].checkedFlag = !itemsModel[indexPath.row].checkedFlag
         saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
@@ -58,6 +65,7 @@ class ToDoTableViewController: UITableViewController {
             item.itemName = textEntered.text
             item.checkedFlag = false
             self.itemsModel.append(item)
+            self.saveItems()
             self.tableView.reloadData()
         }
         
@@ -80,58 +88,32 @@ class ToDoTableViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request: NSFetchRequest<ItemModel> = ItemModel.fetchRequest()
+    func loadItems(with request: NSFetchRequest<ItemModel> = ItemModel.fetchRequest()) {
         do {
             itemsModel = try databaseContext.fetch(request)
         } catch {
             print("Error \(error)")
         }
+        tableView.reloadData()
+    }
+}
+
+extension TodoViewController : UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<ItemModel> = ItemModel.fetchRequest()
+        request.predicate = NSPredicate(format: "itemName CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "itemName", ascending: true)]
+        loadItems(with: request)
     }
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
